61f8fc88d2f4cf325d0dd9c228237a291ea1aec0
Arindam Hansda <arindamhansda484@gmail.com>
2024-02-26 16:20:40 +0530

    Modify LSP and todo-comment icons

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,60 @@
+-----------------------------------------------------------
+-- todo-comments.nvim configuration file
+----------------------------------------------------------
+
+-- Plugin: todo-comments.nvim
+-- url: https://github.com/folke/todo-comments.nvim
+
+local status_ok, todo_comments = pcall(require, 'todo-comments')
+if not status_ok then
+    return
+end
+
+todo_comments.setup {
+    sign_priority = 8,
+    keywords = {
+        FIX  = { icon = " ", color = "error", alt = { "FIXME", "BUG", "FIXIT", "ISSUE" } },
+        TODO = { icon = " ", color = "info" },
+        HACK = { icon = " ", color = "warning" },
+        WARN = { icon = " ", color = "warning", alt = { "WARNING", "XXX" } },
+        PERF = { icon = " ", alt = { "OPTIM", "PERFORMANCE", "OPTIMIZE" } },
+        NOTE = { icon = "🗉 ", color = "hint", alt = { "INFO" } },
+        TEST = { icon = "⏲ ", color = "test", alt = { "TESTING", "PASSED", "FAILED" } },
+    },
+    gui_style = {
+        fg = "NONE",
+        bg = "BOLD",
+    },
+    merge_keywords = true,
+    highlight = {
+        multiline = true,
+        multiline_pattern = "^.",
+        multiline_context = 10,
+        before = "",
+        keyword = "wide",
+        after = "fg",
+        pattern = [[.*<(KEYWORDS)\s*:]],
+        comments_only = true,
+        max_line_len = 400,
+        exclude = {},
+    },
+    colors = {
+        error = { "DiagnosticError", "ErrorMsg", "#DC2626" },
+        warning = { "DiagnosticWarn", "WarningMsg", "#FBBF24" },
+        info = { "DiagnosticInfo", "#2563EB" },
+        hint = { "DiagnosticHint", "#10B981" },
+        default = { "Identifier", "#7C3AED" },
+        test = { "Identifier", "#FF00FF" }
+    },
+    search = {
+        command = "rg",
+        args = {
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+        },
+        pattern = [[\b(KEYWORDS):]],
+    },
+}
