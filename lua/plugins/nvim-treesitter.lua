0c7b7fbf0d7279f5af8701911843d4f40e7a78d8
Arindam Hansda <arindamhansda484@gmail.com>
2026-04-27 13:30:05 +0530

    feat(treesitter): preinstall common language parsers on startup

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,9 +2,29 @@
 -- Treesitter configuration file (nvim-treesitter main branch, Neovim 0.12+)
 -----------------------------------------------------------
 
-local status_ok, _ = pcall(require, "nvim-treesitter")
+local status_ok, nvim_treesitter = pcall(require, "nvim-treesitter")
 if not status_ok then
 	return
+end
+
+if nvim_treesitter.install then
+	nvim_treesitter.install({
+		"bash",
+		"c",
+		"css",
+		"html",
+		"javascript",
+		"typescript",
+		"tsx",
+		"json",
+		"lua",
+		"python",
+		"vim",
+		"yaml",
+		"markdown",
+		"markdown_inline",
+		"java",
+	})
 end
 
 vim.api.nvim_create_autocmd("FileType", {
