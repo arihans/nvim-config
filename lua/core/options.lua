6c9dfb16730520f15f63b3326d45ca8e96541bac
Arindam Hansda <arindamhansda484@gmail.com>
2026-04-27 13:12:28 +0530

    perf(options): disable unused providers to eliminate file-open delay

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,6 +4,11 @@
 local g = vim.g       -- Global variables
 local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
 
+g.loaded_python3_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_perl_provider = 0
+g.loaded_node_provider = 0
+
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
